94625f03aea27c42270c710729058928fe9680d0
daniprec <daniel.precioso@uca.es>
2023-01-23 11:54:52 +0100

    Two more methods for spherical geometry

--- hybrid_routing/geometry/spherical.py
+++ hybrid_routing/geometry/spherical.py
@@ -9,9 +9,9 @@ RAD2M = RADIUS / (2 * np.pi)  # Radians to meters conversion
 DEG2RAD = np.pi / 180
 
 
-def lonlatunitvector(p: Tuple[float]) -> np.array:
+def lonlatunitvector(p: Tuple[float]) -> np.ndarray:
     lon, lat = p[0], p[1]
-    return np.array([np.cos(lon) * np.cos(lat), np.sin(lon) * np.cos(lat), np.sin(lat)])
+    return np.stack([np.cos(lon) * np.cos(lat), np.sin(lon) * np.cos(lat), np.sin(lat)])
 
 
 class Spherical(Geometry):
@@ -23,7 +23,9 @@ class Spherical(Geometry):
 
     def dist_p0_to_p1(self, p0: Tuple[float], p1: Tuple[float]) -> float:
         """Compute the distance between two points, defined in radians. Returns meters."""
-        return RADIUS * np.arccos(np.dot(lonlatunitvector(p0), lonlatunitvector(p1)))
+        return RADIUS * np.arccos(
+            (lonlatunitvector(p0) * lonlatunitvector(p1)).sum(axis=0)
+        )
 
     def angle_p0_to_p1(self, p0: Tuple[float], p1: Tuple[float]) -> float:
         """Return angle (in radians) between two points, w.r.t. X-axis. Returns radians."""
@@ -35,3 +37,25 @@ class Spherical(Geometry):
         gd = self.dist_p0_to_p1(p0, p1)
         vector = np.nan_to_num(gvec * gd / np.sqrt(gvec**2), 0)
         return np.arctan2(vector[1], vector[0])
+
+    def dist_between_coords(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Return euclidean distance (meters) between each set of points (radians)"""
+        p = np.stack([x, y])
+        p0 = p[..., :-1]
+        p1 = p[..., 1:]
+        return self.dist_p0_to_p1(p0, p1)
+
+    def ang_between_coords(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Return angle (in radians) between each set of points (radians)"""
+        p = np.stack([x, y])
+        p0 = p[..., :-1]
+        p1 = p[..., 1:]
+        return self.angle_p0_to_p1(p0, p1)
+
+    def components_to_ang_mod(self, x: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray]:
+        """Gets vector components, returns angle (rad) and module"""
+        pass
+
+    def ang_mod_to_components(self, a: np.ndarray, m: np.ndarray) -> Tuple[np.ndarray]:
+        """Gets angle (rad) and module, returns vector components"""
+        pass
--- pytest/test_spherical.py
+++ pytest/test_spherical.py
@@ -38,3 +38,19 @@ def test_angle_p0_to_p1(p0: Tuple[float], p1: Tuple[float], a: float):
     p1 = [x * DEG2RAD for x in p1]
     ang = Spherical().angle_p0_to_p1(p0, p1)
     np.testing.assert_allclose(ang, a, rtol=0.1)
+
+
+def test_dist_between_coords():
+    lon = np.array([-1.1, -1, 0, 0, 2, 1]) * DEG2RAD
+    lat = np.array([-1, 0, 0, 0, 0, 2]) * DEG2RAD
+    d = Spherical().dist_between_coords(lon, lat)
+    expected = np.array([120, 111, 0, 222, 249]) * 1000
+    np.testing.assert_allclose(d, expected, rtol=0.1)
+
+
+def test_angle_between_coords():
+    lon = np.array([0, 1, 0, 0, 0]) * DEG2RAD
+    lat = np.array([0, 0, 0, 1, 0]) * DEG2RAD
+    angle = Spherical().ang_between_coords(lon, lat)
+    expected = np.array([0, np.pi, np.pi / 2, -np.pi / 2])
+    np.testing.assert_allclose(angle, expected, rtol=0.001)
