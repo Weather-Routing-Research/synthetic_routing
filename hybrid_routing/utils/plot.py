565752a9ed2c8acd3d6a892eb82e6275a8d91cda
Daniel Precioso <danipg1409@gmail.com>
2023-02-03 22:03:30 +0100

    Add extent param

--- hybrid_routing/utils/plot.py
+++ hybrid_routing/utils/plot.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
@@ -5,7 +7,9 @@ from matplotlib.axes import Axes
 from hybrid_routing.geometry import DEG2RAD
 
 
-def plot_ticks_radians_to_degrees(step: float = 5):
+def plot_ticks_radians_to_degrees(
+    step: float = 5, extent: Optional[List[float]] = None
+):
     """Changes the ticks from radians to degrees
 
     Parameters
@@ -14,8 +18,11 @@ def plot_ticks_radians_to_degrees(step: float = 5):
         Step in degrees, by default 5
     """
     ax: Axes = plt.gca()
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
+    if extent is None:
+        xmin, xmax = ax.get_xlim()
+        ymin, ymax = ax.get_ylim()
+    else:
+        xmin, xmax, ymin, ymax = extent
     # Radians to degrees
     xticks = np.arange(xmin, xmax, step * DEG2RAD)
     plt.xticks(xticks, labels=[f"{i:.1f}" for i in xticks / DEG2RAD])
