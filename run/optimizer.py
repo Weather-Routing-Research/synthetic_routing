5d53ec25c8d25e8fe10d6529863cec241159f082
Daniel Precioso <danipg1409@gmail.com>
2023-02-10 19:41:52 +0100

    GIF can be plotted with real data

--- run/optimizer.py
+++ run/optimizer.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import typer
 
+from hybrid_routing.geometry import DEG2RAD
 from hybrid_routing.jax_utils.optimize import Optimizer
-from hybrid_routing.vectorfields import *
+from hybrid_routing.vectorfields import VectorfieldReal
 from hybrid_routing.vectorfields.base import Vectorfield
 
 
@@ -23,13 +24,29 @@ def main(
     num_angles: int = 20,
     vel: float = 1,
     dist_min: float = 0.1,
+    do_color: bool = False,
     path_out: str = "output/",
 ):
-    vectorfield: Vectorfield = eval(vf)()
-    if discretized:
-        vectorfield = vectorfield.discretize(-1, 7, -1, 7)
-    q0 = (0, 0)
-    q1 = (6, 2)
+    # Import vectorfield
+    if vf == "Real":
+        vectorfield = VectorfieldReal.from_folder("./data", "real-land", radians=True)
+        q0 = (43.49 * DEG2RAD, -1.66 * DEG2RAD)
+        q1 = (98.14 * DEG2RAD, 10.21 * DEG2RAD)
+        xlim = (40 * DEG2RAD, 100 * DEG2RAD)
+        ylim = (-25 * DEG2RAD, 30 * DEG2RAD)
+        step = DEG2RAD
+    else:
+        module = __import__("hybrid_routing")
+        module = getattr(module, "vectorfields")
+        vectorfield: Vectorfield = getattr(module, vf)()
+        if discretized:
+            vectorfield = vectorfield.discretize(-1, 7, -1, 7)
+        q0 = (0, 0)
+        q1 = (6, 2)
+        xlim = (-1, 7)
+        ylim = (-1, 7)
+        step = 1
+
     optimizer = Optimizer(
         vectorfield,
         time_iter=time_iter,
@@ -47,6 +64,8 @@ def main(
     title += " Runge-Kutta" if use_rk else " ODEINT"
     title += " " + optimizer.method
 
+    color = "white" if do_color else "green"
+
     # Build folder with images
     path_img = Path("img_optim")
     if not path_img.exists():
@@ -55,13 +74,20 @@ def main(
     idx = 0
     # Generate one plot every 2 iterations
     for list_routes in optimizer.optimize_route(q0, q1):
-        vectorfield.plot(x_min=-1, x_max=7, y_min=-1, y_max=7)
+        vectorfield.plot(
+            x_min=xlim[0],
+            x_max=xlim[1],
+            y_min=ylim[0],
+            y_max=ylim[1],
+            step=step,
+            do_color=do_color,
+        )
         for route in list_routes:
-            plt.plot(route.x, route.y, color="green", alpha=0.6)
+            plt.plot(route.x, route.y, color=color, alpha=0.6)
         plt.scatter([q0[0], q1[0]], [q0[1], q1[1]], c="red")
         plt.title(title)
-        plt.xlim(-1, 7)
-        plt.ylim(-1, 7)
+        plt.xlim(xlim[0], xlim[1])
+        plt.ylim(ylim[0], ylim[1])
         fout = path_img / f"{idx:03d}.png"
         idx += 1
         plt.savefig(fout)
@@ -71,12 +97,19 @@ def main(
     # Plot best for some frames
     for i in range(6):
         route = list_routes[0]
-        vectorfield.plot(x_min=-1, x_max=7, y_min=-1, y_max=7)
-        plt.plot(route.x, route.y, color="green", alpha=1)
+        vectorfield.plot(
+            x_min=xlim[0],
+            x_max=xlim[1],
+            y_min=ylim[0],
+            y_max=ylim[1],
+            step=step,
+            do_color=do_color,
+        )
+        plt.plot(route.x, route.y, color=color, alpha=1)
         plt.scatter([q0[0], q1[0]], [q0[1], q1[1]], c="red")
         plt.title(title)
-        plt.xlim(-1, 7)
-        plt.ylim(-1, 7)
+        plt.xlim(xlim[0], xlim[1])
+        plt.ylim(ylim[0], ylim[1])
         fout = path_img / f"{idx:03d}.png"
         idx += 1
         plt.savefig(fout)
