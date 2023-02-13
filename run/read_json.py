4133728576f856537f07ae2ac0a1415aed81f92b
Daniel Precioso <danipg1409@gmail.com>
2023-02-13 17:35:49 +0100

    Improve read json script

--- run/read_json.py
+++ run/read_json.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import typer
@@ -10,28 +11,18 @@ from hybrid_routing.pipeline import Pipeline
 
 def main(
     path_json: str,
-    key: str = "route_zivp",
-    path_out: str = "output",
+    path_out: Optional[str] = None,
     time_step: float = 3600,
-    num_iter: int = 200,
+    num_iter: int = 2000,
 ):
-    path_out: Path = Path(path_out)
     path_json: Path = Path(path_json)
 
     with open(path_json) as file:
         dict_json: dict = json.load(file)
 
-    name = path_json.stem
+    # Route from ZIVP
+    dict_route = dict_json["route_zivp"]
 
-    print("\n---\nBenchmark, run:", name)
-
-    # Try to generate the route
-    try:
-        dict_route = dict_json[key]
-    except KeyError:
-        raise KeyError(
-            "Key not found in dict. Available are: ", ", ".join(dict_json.keys())
-        )
     route = Route(
         x=dict_route["x"],
         y=dict_route["y"],
@@ -40,18 +31,46 @@ def main(
         geometry=dict_route["geometry"],
     )
 
-    path = "./data" if bool(dict_json["real"]) else None
-    pipe = Pipeline(
-        p0=(route.x[0], route.y[0]),
-        pn=(route.x[-1], route.y[-1]),
-        key=dict_json["key"],
-        path=path,
+    print("ZIVP")
+    print(f"  Time (h): {route.t[-1] / 3600:.1f}")
+    print(f"  Distance (km): {route.d.sum() / 1000:.1f}")
+
+    # Route from DNJ
+    dict_route = dict_json["route_dnj"]
+
+    route_dnj = Route(
+        x=dict_route["x"],
+        y=dict_route["y"],
+        t=dict_route["t"],
+        theta=dict_route["theta"],
+        geometry=dict_route["geometry"],
     )
-    pipe.add_route(route, vel=dict_json["vel"])
-    pipe.solve_dnj(num_iter=num_iter, time_step=time_step, optimize_for="time")
-    pipe.plot()
-    plt.savefig(path_out / (name.lower().replace(" ", "_") + ".png"))
-    plt.close()
+
+    print("\nDNJ")
+    print(f"  Time (h): {route_dnj.t[-1] / 3600:.1f}")
+    print(f"  Distance (km): {route_dnj.d.sum() / 1000:.1f}")
+
+    if path_out:
+        path_out: Path = Path(path_out)
+        path = "./data" if bool(dict_json["real"]) else None
+        pipe = Pipeline(
+            p0=(route.x[0], route.y[0]),
+            pn=(route.x[-1], route.y[-1]),
+            key=dict_json["key"],
+            path=path,
+        )
+        pipe.add_route(route, vel=dict_json["vel"])
+        print(f"\nDNJ redo, {num_iter} iterations")
+        pipe.solve_dnj(num_iter=num_iter, time_step=time_step, optimize_for="time")
+
+        route_dnj = pipe.route_dnj
+        print(f"  Time (h): {route_dnj.t[-1] / 3600:.1f}")
+        print(f"  Distance (km): {route_dnj.d.sum() / 1000:.1f}")
+
+        pipe.plot()
+        name = path_json.stem
+        plt.savefig(path_out / (name.lower().replace(" ", "_") + ".png"))
+        plt.close()
 
 
 if __name__ == "__main__":
