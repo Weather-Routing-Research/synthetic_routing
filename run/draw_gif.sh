8dd4186ff76184b741e881372324b8ad3fd15af5
daniprec <daniel.precioso@uca.es>
2023-02-17 16:53:20 +0100

    Add draw gif sh call

---
+++ run/draw_gif.sh
@@ -0,0 +1,5 @@
+python run/draw_gif.py  --vf Circular --vel 1 --time-iter 0.1 --time-step 0.01
+python run/draw_gif.py  --vf FourVortices --vel 1 --time-iter 0.1 --time-step 0.01
+python run/draw_gif.py  --vf Real --vel 3 --time-iter 14400 --time-step 600 --do-color
+python run/draw_gif.py  --vf Real --vel 6 --time-iter 14400 --time-step 600  --do-color
+python run/draw_gif.py  --vf Real --vel 10 --time-iter 14400 --time-step 600 --do-color
