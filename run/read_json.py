563733714e37dd363fd1039aaed5e8066f767494
Daniel Precioso <danipg1409@gmail.com>
2023-02-20 14:29:25 +0100

    Read json plots comparison

--- run/read_json.py
+++ run/read_json.py
@@ -7,15 +7,14 @@ import typer
 
 from hybrid_routing.optimization import Route
 from hybrid_routing.pipeline import Pipeline
+from hybrid_routing.utils.plot import plot_routes
 
 
-def main(
-    path_json: str,
-    path_out: Optional[str] = None,
-    num_iter: int = 2000,
+def open_json_and_plot(
+    path_json: Path,
+    path_out: Path = "output",
+    num_iter: Optional[int] = None,
 ):
-    path_json: Path = Path(path_json)
-
     with open(path_json) as file:
         dict_json: dict = json.load(file)
 
@@ -30,6 +29,17 @@ def main(
         geometry=dict_route["geometry"],
     )
 
+    # Load pipeline
+    path = "./data" if bool(dict_json["real"]) else None
+    pipe = Pipeline(
+        p0=(route.x[0], route.y[0]),
+        pn=(route.x[-1], route.y[-1]),
+        key=dict_json["key"],
+        path=path,
+    )
+    pipe.add_route(route, vel=dict_json["vel"])
+
+    route.recompute_times(pipe.vel, pipe.vectorfield)
     print("ZIVP")
     print(f"  Time (h): {route.t[-1] / 3600:.1f}")
     print(f"  Distance (km): {route.d.sum() / 1000:.1f}")
@@ -44,50 +54,60 @@ def main(
         theta=dict_route["theta"],
         geometry=dict_route["geometry"],
     )
-
+    route_dnj.recompute_times(pipe.vel, pipe.vectorfield)
     print("\nDNJ")
     print(f"  Time (h): {route_dnj.t[-1] / 3600:.1f}")
     print(f"  Distance (km): {route_dnj.d.sum() / 1000:.1f}")
 
     # Route from geodesic
-    try:
-        dict_route = dict_json["route"]["geodesic"]
+    dict_route = dict_json["route"]["geodesic"]
 
-        route_geod = Route(
-            x=dict_route["x"],
-            y=dict_route["y"],
-            t=dict_route["t"],
-            theta=dict_route["theta"],
-            geometry=dict_route["geometry"],
-        )
+    route_geod = Route(
+        x=dict_route["x"],
+        y=dict_route["y"],
+        t=dict_route["t"],
+        theta=dict_route["theta"],
+        geometry=dict_route["geometry"],
+    )
+    route_geod.recompute_times(pipe.vel, pipe.vectorfield)
+    print("\nGeodesic")
+    print(f"  Time (h): {route_geod.t[-1] / 3600:.1f}")
+    print(f"  Distance (km): {route_geod.d.sum() / 1000:.1f}")
 
-        print("\nGeodesic")
-        print(f"  Time (h): {route_geod.t[-1] / 3600:.1f}")
-        print(f"  Distance (km): {route_geod.d.sum() / 1000:.1f}")
-    except KeyError:
-        pass
+    plot_routes([route, route_dnj, route_geod], pipe.vectorfield, vel=pipe.vel)
+    name = path_json.stem
+    plt.savefig(path_out / (name.lower().replace(" ", "_") + "_compare.png"))
+    plt.close()
 
-    if path_out:
-        path_out: Path = Path(path_out)
-        path = "./data" if bool(dict_json["real"]) else None
-        pipe = Pipeline(
-            p0=(route.x[0], route.y[0]),
-            pn=(route.x[-1], route.y[-1]),
-            key=dict_json["key"],
-            path=path,
-        )
-        pipe.add_route(route, vel=dict_json["vel"])
+    if num_iter:
         print(f"\nDNJ redo, {num_iter} iterations")
         pipe.solve_dnj(num_iter=num_iter, optimize_for="time")
 
         route_dnj = pipe.route_dnj
         print(f"  Time (h): {route_dnj.t[-1] / 3600:.1f}")
         print(f"  Distance (km): {route_dnj.d.sum() / 1000:.1f}")
-
         pipe.plot()
-        name = path_json.stem
-        plt.savefig(path_out / (name.lower().replace(" ", "_") + ".png"))
+
+        plt.savefig(name.lower().replace(" ", "_") + "_dnj.png")
         plt.close()
+
+
+def main(
+    path_in: str = "output",
+    path_out: str = "output",
+    num_iter: Optional[int] = None,
+):
+    path_in: Path = Path(path_in)
+    path_out: Path = Path(path_out)
+
+    if path_in.is_dir():
+        list_json = [f for f in path_in.iterdir() if f.name.endswith("json")]
+    else:
+        list_json = [path_in]
+
+    for path_json in list_json:
+        print(f"\n-----\n{path_json.name}\n")
+        open_json_and_plot(path_json, path_out=path_out, num_iter=num_iter)
 
 
 if __name__ == "__main__":
