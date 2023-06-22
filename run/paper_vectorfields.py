b7d5c3a19dc42ae8a2a8a2539f669fc7dea854ce
daniprec <daniel.precioso@uca.es>
2023-06-22 10:11:48 +0200

    Add synthetic vector fields

--- run/paper_vectorfields.py
+++ run/paper_vectorfields.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -7,19 +8,33 @@ from hybrid_routing.geometry import DEG2RAD
 from hybrid_routing.utils.config import load_config
 from hybrid_routing.utils.plot import plot_ticks_radians_to_degrees
 from hybrid_routing.vectorfields import VectorfieldReal
+from hybrid_routing.vectorfields.base import Vectorfield
 
-list_benchmark = load_config("data/config.toml", "real").tolist()
+path_cfg = "data/config.toml"
+
+list_benchmark = (
+    load_config(path_cfg, "synthetic").tolist() + load_config(path_cfg, "real").tolist()
+)
 
 path_data = "./data"
 path_out = Path("output/")
 
 for dict_vf in list_benchmark:
-    p0 = np.array(dict_vf["p0"]) * DEG2RAD
-    pn = np.array(dict_vf["pn"]) * DEG2RAD
     key = dict_vf["key"]
-    vf = VectorfieldReal.from_folder(dict_vf["path"], key, radians=dict_vf["si_units"])
-    vf.plot(step=DEG2RAD, do_color=True)
-    plot_ticks_radians_to_degrees(step=6)
+    try:
+        vf = VectorfieldReal.from_folder(
+            dict_vf["path"], key, radians=dict_vf["si_units"]
+        )
+        p0 = np.array(dict_vf["p0"]) * DEG2RAD
+        pn = np.array(dict_vf["pn"]) * DEG2RAD
+        vf.plot(step=DEG2RAD, do_color=True)
+        plot_ticks_radians_to_degrees(step=6)
+    except KeyError:
+        module = import_module("hybrid_routing.vectorfields")
+        vf: Vectorfield = getattr(module, key)()
+        p0 = np.array(dict_vf["p0"])
+        pn = np.array(dict_vf["pn"])
+        vf.plot(step=0.5, extent=dict_vf["plot"]["extent"])
     plt.scatter(p0[0], p0[1], s=50, c="black")
     plt.scatter(p0[0], p0[1], s=25, c="y")
     plt.scatter(pn[0], pn[1], s=50, c="black")
